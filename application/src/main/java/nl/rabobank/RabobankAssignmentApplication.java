1dcddbee15f9615a38ccf380ba0e74ed33f38fc3
Fred Oliveira <faquino@uolinc.com>
2021-08-22 13:40:33 -0300

    Refactoring Sentry configuration.

--- application/src/main/java/nl/rabobank/RabobankAssignmentApplication.java
+++ application/src/main/java/nl/rabobank/RabobankAssignmentApplication.java
@@ -1,6 +1,5 @@
 package nl.rabobank;
 
-import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,12 +8,6 @@ public class RabobankAssignmentApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(RabobankAssignmentApplication.class, args);
-
-    Sentry.init(options -> {
-      options.setDsn("https://155ba8755a504cc38377790f74aaba96@o968895.ingest.sentry.io/5920155");
-      options.setTracesSampleRate(1.0);
-      options.setDebug(true);
-    });
   }
 
 }
