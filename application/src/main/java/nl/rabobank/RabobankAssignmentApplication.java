11ce19e47a3ea9ac0e54ff6e14486da86029da3e
Fred Oliveira <faquino@uolinc.com>
2021-08-22 10:15:07 -0300

    Configuring Sentry to report exceptions and spring validator.

--- application/src/main/java/nl/rabobank/RabobankAssignmentApplication.java
+++ application/src/main/java/nl/rabobank/RabobankAssignmentApplication.java
@@ -1,5 +1,6 @@
 package nl.rabobank;
 
+import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,12 @@ public class RabobankAssignmentApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(RabobankAssignmentApplication.class, args);
+
+    Sentry.init(options -> {
+      options.setDsn("https://155ba8755a504cc38377790f74aaba96@o968895.ingest.sentry.io/5920155");
+      options.setTracesSampleRate(1.0);
+      options.setDebug(true);
+    });
   }
 
 }
