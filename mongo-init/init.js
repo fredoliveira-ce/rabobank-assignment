f626b2921a017a2a57634de040cab40a83334a7e
Fred Oliveira <faquino@uolinc.com>
2021-08-22 20:30:58 -0300

    Adding new fake users to initiate the database.

--- mongo-init/init.js
+++ mongo-init/init.js
@@ -1,5 +1,8 @@
 conn = new Mongo();
 db = conn.getDB("robobank");
 
-db.user.insert({ "username": "admin", "password": "pwd", "role": "ADMIN" });
-db.user.insert({ "username": "user", "password": "usr", "role": "USER" });
+db.user.insert({ "username": "admin", "password": "pwd", "document": "00000000000", "role": "ADMIN" });
+db.user.insert({ "username": "peggy.burns", "password": "usr", "document": "109327486", "role": "USER" });
+db.user.insert({ "username": "randall.gordon", "password": "usr", "document": "50293847502", "role": "USER" });
+db.user.insert({ "username": "douglas.sanchez", "password": "usr", "document": "7509823475", "role": "USER" });
+db.user.insert({ "username": "michele.clark", "password": "usr", "document": "652794353", "role": "USER" });
